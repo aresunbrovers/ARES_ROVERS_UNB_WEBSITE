03ec061e2e680cdc47239ab2d0836e9b5a307623
Blazemorales <cunhajp2006@gmail.com>
2026-05-27 17:20:00 -0300

    subindo

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  output: 'export',
-  // Substitua pelo nome exato do seu repositório
-  basePath: '/ARES_ROVERS_UNB_WEBSITE',
-  assetPrefix: '/ARES_ROVERS_UNB_WEBSITE/',
-  // Desabilita otimização de imagem, pois GitHub Pages não suporta o formato otimizado do Next
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/ARES_ROVERS_UNB_WEBSITE",
+  assetPrefix: "/ARES_ROVERS_UNB_WEBSITE/",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
 };
-
-module.exports = nextConfig;   
 
 export default nextConfig;
