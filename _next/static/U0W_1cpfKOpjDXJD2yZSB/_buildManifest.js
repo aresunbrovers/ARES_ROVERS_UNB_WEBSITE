814978fc754f22cd06a966006c85b025bbd58f0c
Blazemorales <cunhajp2006@gmail.com>
2026-05-27 17:28:17 -0300

    Updates

---
+++ _next/static/U0W_1cpfKOpjDXJD2yZSB/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ARES_ROVERS_UNB_WEBSITE//_next/:path+",
+        "destination": "/ARES_ROVERS_UNB_WEBSITE/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
